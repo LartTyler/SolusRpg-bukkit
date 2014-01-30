29c9ec8a31735a9a5b1c523b04a6d46d7e77637a
Tyler Lartonoix <larttyler@live.com>
2014-01-30 17:38:29 -0500

    More work in progress

---
+++ src/me/dbstudios/solusrpg/entities/meters/HealthMeter.java
@@ -0,0 +1,14 @@
+package me.dbstudios.solusrpg.entities.meters;
+
+import me.dbstudios.solusrpg.util.math.Expression;
+
+public interface HealthMeter implements VitalMeter {
+	private Expression regenFormula;
+	private long regenRate;
+	private int max;
+	private int current;
+
+	public HealthMeter(int max, String regenFormula, long regenRate) {
+
+	}
+}
