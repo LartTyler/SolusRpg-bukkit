9527375bf0d3682e9955d4ad22ad9a14e292ce84
Tyler Lartonoix <larttyler@live.com>
2014-02-03 00:28:44 -0500

    Work in progress

--- src/me/dbstudios/solusrpg/entities/meters/HealthMeter.java
+++ src/me/dbstudios/solusrpg/entities/meters/HealthMeter.java
@@ -4,11 +4,52 @@ import me.dbstudios.solusrpg.util.math.Expression;
 
 public class HealthMeter implements VitalMeter {
 	private Expression regenFormula;
+	private String name;
+	private int regenAmount;
 	private long regenRate;
 	private int max;
 	private int current;
 
-	public HealthMeter(int max, String regenFormula, long regenRate) {
+	public HealthMeter(int max, String regenFormula, long regenRate, String meterName) {
+		this.max = max;
+		this.current = max;
+		this.regenFormula = new Expression(regenFormula);
+		this.regenRate = regenRate;
+		this.name = meterName;
 
+		this.regenAmount = this.regenFormula
+			.clearParameters()
+			.setParameter("max_health", max)
+			.eval();
+	}
+
+	public int get() {
+		return this.current;
+	}
+
+	public VitalMeter set(int current) {
+		this.current = current;
+
+		return this;
+	}
+
+	public int getMax() {
+		return this.max;
+	}
+
+	public VitalMeter setMax(int max) {
+		this.max = max;
+
+		return this;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public VitalMeter setName(String meterName) {
+		this.name = meterName;
+
+		return this;
 	}
 }
