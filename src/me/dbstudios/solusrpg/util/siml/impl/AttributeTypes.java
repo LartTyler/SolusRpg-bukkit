a96cafa9e0de9ba60cb1086847590f58f44d7f54
Tyler Lartonoix <larttyler@live.com>
2014-02-17 11:11:13 -0500

    WIP

--- src/me/dbstudios/solusrpg/util/siml/impl/AttributeTypes.java
+++ src/me/dbstudios/solusrpg/util/siml/impl/AttributeTypes.java
@@ -7,17 +7,21 @@ public enum AttributeTypes {
 	COLOR(new ColorAttributeType()),
 	STRING(new StringAttributeType());
 
-	private final AttributeType type;
+	private final AttributeType<?> type;
 
-	private AttributeTypes(AttributeType type) {
+	private AttributeTypes(AttributeType<?> type) {
 		this.type = type;
 	}
 
-	public AttributeType getType() {
+	public AttributeType<?> getType() {
 		return this.type;
 	}
 
 	public boolean test(String value) {
 		return type.test(value);
+	}
+
+	public boolean isSameType(AttributeType<?> type) {
+		return this.type.getClass().isInstance(type);
 	}
 }
