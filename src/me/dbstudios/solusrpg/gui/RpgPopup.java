903102c51451a2f4c29d1271d073ecfe382a748b
Tyler Lartonoix <larttyler@live.com>
2014-03-07 12:16:14 -0500

    Not sure what I was planning on using this for...

--- src/me/dbstudios/solusrpg/gui/RpgPopup.java
+++
@@ -1,9 +0,0 @@
-package me.dbstudios.solusrpg.gui;
-
-import me.dbstudios.solusrpg.entities.player.RpgPlayer;
-
-public interface RpgPopup {
-	public RpgPopup attachTo(RpgPlayer player);
-	public RpgPopup attachTo(RpgPlayer player, Method callback);
-	public RpgPopup removeFrom(RpgPlayer player);
-}
