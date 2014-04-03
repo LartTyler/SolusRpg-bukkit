afbdc4e798bf74e84adc3fe7d3742979352dc8b4
Tyler Lartonoix <larttyler@live.com>
2014-04-03 01:08:34 -0400

    WIP

--- src/me/dbstudios/solusrpg/RpgPopupFactory.java
+++ src/me/dbstudios/solusrpg/RpgPopupFactory.java
@@ -2,4 +2,21 @@ package me.dbstudios.solusrpg;
 
 public class RpgPopupFactory {
 	private static final Map<String, RpgPopup> popups = new HashMap<>();
+
+	protected static void registerPopup(String name, RpgPopup popup) {
+		popups.put(name, popup);
+	}
+
+	public static RpgPopup getPopup(String name) {
+		return RpgPopupFactory.getPopup(name, RpgPopup.class);
+	}
+
+	public static <T extends RpgPopup> T getPopup(String name, Class<T> cl) {
+		RpgPopup popup = popups.get(name);
+
+		if (cl.isInstance(popup))
+			return cl.cast(popup);
+		
+		return null;
+	}
 }
